32a283717435ee80603f98b3fac8cad546c30134
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-04-30 13:49:15 -0400

    Add apple and elephant

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,29 @@
 import greenfoot.*;
 
+/**
+ * The World our hero lives in.
+ * 
+ * @author Kung
+ * @version April 2025
+ */
 public class MyWorld extends World {
-    public MyWorld() {
+    public MyWorld() 
+    {  
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject (elephant, 300, 200);
+        
+        createApple();
+    }
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
