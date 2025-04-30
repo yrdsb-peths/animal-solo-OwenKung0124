cd63ead17840b9aea8b4d81628b6707aad391af4
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-04-30 14:33:28 -0400

    Add number

--- MyWorld.java
+++ MyWorld.java
@@ -9,10 +9,16 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() 
     {  
+        //Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
+        //Create the elephant object
         Elephant elephant = new Elephant();
         addObject (elephant, 300, 200);
+        
+        //Create a label
+        Label scoreLabel = new Label(0, 80);
+        addObject( scoreLabel, 50, 50);
         
         createApple();
     }
